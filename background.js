3092aae785eb658ba2d9c7cb39199e27772c152c
aomerk <aliomerkeser@gmail.com>
2019-10-15 13:29:56 +0300

    en language support added

--- background.js
+++ background.js
@@ -7,12 +7,20 @@
  * *****
  *  ******/
 
-
+/****
+ * *
+ *
+ *  TODO Birisi Regex ile yapmak isterse, lutfen yapsin. Ben de gormus olurum, kafami toparlayamadim suan Regex icin.
+ *     sadece tr ve en icin calisiyor.
+ *     evet, bastaki prefixi alip wiki/prefix yapilabilir.
+ *
+ ****/
 chrome.webRequest.onBeforeRequest.addListener(
     function (details) {
         let changingUrl = details.url;
-        if (changingUrl.includes("tr.wikipedia")) {
-            changingUrl = changingUrl.replace('tr.wikipedia', 'tr.0wikipedia');
+        if (changingUrl.includes(".wikipedia")) {
+            changingUrl = changingUrl.replace('tr.wikipedia.org/wiki', 'wikizeroo.org/wiki/tr');
+            changingUrl = changingUrl.replace('en.wikipedia.org/wiki', 'wikizeroo.org/wiki/en');
             chrome.tabs.update({url: changingUrl});
         }
     }, {urls: ["<all_urls>"]}, ["blocking"]
